74bd290f238d64b3c7494a1142e287ebe27bc23a
Jim Cooper <jim@cooperplanet.com>
2023-04-25 13:34:48 -0600

    m3-CreatingAngularComponents: Complete

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}

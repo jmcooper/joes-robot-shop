79221a5c6620601f72cf7d8587949560fcb2fe95
Joe Eames <joeeames@gmail.com>
2023-05-08 22:20:19 -0600

    Revert "changed code"
    This reverts commit 6d157ea2f1fb0b70d1e8d9af618b1c78d7c248be.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bot-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+}
--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
-  styleUrls: ['./site-header.component.css'],
+  styleUrls: ['./site-header.component.css']
 })
 export class SiteHeaderComponent {
-  constructor() {}
+
+  constructor() { }
+
 }

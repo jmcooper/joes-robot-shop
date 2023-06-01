0b3c43eab680847d2a5195866d0e40f8c3dc72bf
Jim Cooper <jim@cooperplanet.com>
2023-06-01 12:09:35 -0600

    m9-c5:Creating Redirect Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: "Home - Joe's Robot Shop" },
+  { path: 'catalog', component: CatalogComponent, title: "Catalog - Joe's Robot Shop" },
+  { path: 'cart', component: CartComponent, title: "Cart - Joe's Robot Shop" },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [],

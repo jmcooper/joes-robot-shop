ea255a49596b6c32f5972dfe2137fa34a6a436b1
Jim Cooper <jim@cooperplanet.com>
2023-06-05 16:34:35 -0600

    Add form-controls component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './user/template-form-controls/template-form-controls.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SignInComponent } from './user/sign-in/sign-in.component';
     ProductDetailsComponent,
     CartComponent,
     SignInComponent,
+    TemplateFormControlsComponent
   ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],

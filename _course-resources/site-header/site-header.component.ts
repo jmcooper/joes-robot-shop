8b3d34b5e377acf5b9768cf3c9d480f732ee5041
Jim Cooper <jim@cooperplanet.com>
2023-06-06 22:54:30 -0600

    Add course resources

---
+++ _course-resources/site-header/site-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-site-header',
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css']
+})
+export class SiteHeaderComponent {
+
+  constructor() { }
+
+}

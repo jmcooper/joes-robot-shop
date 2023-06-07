c4868748dfeef5d02944fc5ef59f256ab31b4bf4
Jim Cooper <jim@cooperplanet.com>
2023-06-06 23:01:26 -0600

    Update course resources

---
+++ _course-resources/user/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+
+import { IUser, IUserCredentials } from './user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private user: BehaviorSubject<IUser | null>;
+
+  constructor(private http: HttpClient) {
+    this.user = new BehaviorSubject<IUser | null>(null);
+  }
+
+  getUser(): Observable<IUser | null> {
+    return this.user;
+  }
+
+  signIn(credentials: IUserCredentials): Observable<IUser> {
+    return this.http
+      .post<IUser>('/api/sign-in', credentials)
+      .pipe(map((user: IUser) => {
+        this.user.next(user);
+        return user;
+      }));
+  }
+
+  signOut() {
+    this.user.next(null);
+  }
+}
